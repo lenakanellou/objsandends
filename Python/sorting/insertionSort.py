a06bae0b718d5c7395a4a23e718d07bbe7b8cdc4
kanellou <lena.kanellou@gmail.com>
2024-10-03 15:30:03 +0300

    Implemented insertion sort with nested for loops.

--- Python/sorting/insertionSort.py
+++ Python/sorting/insertionSort.py
@@ -1,3 +1,27 @@
+#!/usr/bin/env python3
+
+"""
+Script Name: 	insertionSort.py
+Description: 	Implementation of Insertion Sort on a list-implelmented array of elements, 
+				and accompanying main() function that provides a sample array to be sorted.
+			 	This implementation was created as part of an introductory course on data 
+				structures. 
+				The purpose of this particular implementation is to express the Insertion 
+				Sort algorithm using a nested for loop structure.
+				The reasoning is that such a structure may better illustrate to students 
+				how the comparison of the algorithm to the commonly used example of sorting 
+				a hand of cards one has been dealt, ends up resulting in O(N²) time complexity.  
+Author: 		Lena Kanellou
+Date: 			2024
+Repository: 	https://github.com/lenakanellou/objsandends
+License: 		MIT License (see LICENSE file in the root of the repository)
+
+Usage:
+    python3 insertionSort.py
+
+"""
+
+
 def insertionSort(arr):
 
 	n = len(arr)
@@ -6,18 +30,19 @@ def insertionSort(arr):
 		print("Array is empty!")
 		return None, -1 
 
-	for i in range(1, n) : 
+	for i in range(1, n): 
+		print(f"i is {i}")
 		value = arr[i]
-		j = i-1
-
-		while j >= 0:
-			if value < arr[j] :
+		#j = i-1
+		for j in range(i-1, -1, -1): 
+			print(f"j is {j}")
+			if value < arr[j]:
 				arr[j+1] = arr[j]
-				j = j - 1
+				arr[j] = value
 			else:
+				arr[j+1] = value
 				break
-
-		arr[j+1] = value
+		print(arr)
 				
 	return arr, 0
 
